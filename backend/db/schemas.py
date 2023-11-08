6a44dba1c198d3ad0f9a15e8a25b70ae4cdfa23e
vasilevegor <vasilev.egor163@gmail.com>
2023-11-08 19:56:36 +0100

    Detail View and Schema with FastAPI and Pydantic

--- backend/db/schemas.py
+++ backend/db/schemas.py
@@ -2,7 +2,22 @@ from datetime import date
 from pydantic import BaseModel
 
 
-class FlightPriceSchema(BaseModel): # Pydantic
+class FlightPriceOverviewSchema(BaseModel): # Pydantic
+    id: int
+    flightDate: date
+    startingAirport: str
+    destinationAirport: str
+    # isBasicEconomy: bool
+    # isRefundable: bool
+    # isNonStop: bool
+    segmentsAirlineName: str
+    totalFare: int
+
+    class Config:
+        from_attributes = True
+        
+        
+class FlightPriceDetailSchema(BaseModel): # Pydantic
     id: int
     flightDate: date
     startingAirport: str
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 from ast import List
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.env import config
 from backend.db import utils as db_utils
 from backend.db.connect import get_db_session, SessionLocal
-from backend.db.schemas import FlightPriceSchema
+from backend.db.schemas import FlightPriceOverviewSchema, FlightPriceDetailSchema
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 FRONTEND_ORIGINS = config("FRONTEND_ORIGINS", cast=lambda x: [s.strip() for s in x.split(",")], default="")
@@ -28,6 +28,14 @@ def get_hello():
     }
     
     
-@app.get('/flights/', response_model=list[FlightPriceSchema])
-def read_flight_prices(db_session: SessionLocal = Depends(get_db_session)):
-    return db_utils.get_flight_prices(db_session)
+@app.get('/flights/', response_model=list[FlightPriceOverviewSchema])
+def read_flight_prices(offset: int=0, limit: int=100, db_session: SessionLocal = Depends(get_db_session)):
+    return db_utils.get_flight_prices(db_session, offset=offset, limit=limit)
+
+
+@app.get('/flights/{flight_price}', response_model=FlightPriceDetailSchema)
+def read_flight_prices(flight_price: int, db_session: SessionLocal = Depends(get_db_session)):
+    db_flight_value = db_utils.get_flight_price(db_session, flight_price)
+    if db_flight_value is None:
+        raise HTTPException(status_code=404, detail="Flight price not found")
+    return db_flight_value
