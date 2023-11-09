bbd44b8e10b86cd8c9ca229ec8254f2c9a9b5841
vasilevegor <vasilev.egor163@gmail.com>
2023-11-09 19:56:54 +0100

    Form Styling with Flowbite References

--- src/app/flights/forms.js
+++ src/app/flights/forms.js
@@ -42,36 +42,54 @@ export default function FlightPredictForm(props) {
     
     const btnClassName = predictData.loading ? "btn-disabled" : "btn-primary"
     const btnLabel = predictData.loading ? "Loading..." : "Help me"
-    return <div><form onSubmit={handleSubmit}>
-        <AirportDropdown 
-            name='startingAirport' 
-            value={startAirportVal}
-            onChange={e => setStartAirportVal(e.target.value)} />
-        <AirportDropdown 
-            name='destinationAirport'
-            value={endAirportVal} 
-            onChange={e => setEndAirportVal(e.target.value)} 
-            filterval={startAirportVal}  />
+    return <div><form className="space-y-2 md:space-y-3" onSubmit={handleSubmit}>
+        <div className="grid md:grid-cols-2 md:gap-3">
+            <div>
+                <label htmlFor='startingAirport'>
+                        Starting Airport
+                        </label>
+                    <AirportDropdown 
+                        name='startingAirport' 
+                        value={startAirportVal}
+                        onChange={e => setStartAirportVal(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor='destinationAirport'>
+                        Destination Airport
+                        </label>
+                    <AirportDropdown 
+                        name='destinationAirport'
+                        value={endAirportVal} 
+                        onChange={e => setEndAirportVal(e.target.value)} 
+                        filterval={startAirportVal}  />            
+            </div>            
+        </div>
 
-        <div>
-            <input type='checkbox' name='isNonStop' id='isNonStop' />
-                <label htmlFor='isNonStop'>
-                    Non stop flight?
-                </label>
+
+        <div className="grid md:grid-cols-6 md:gap-3">
+            <div>
+                <input type='checkbox' name='isNonStop' id='isNonStop' />
+                    <label htmlFor='isNonStop'>
+                        Non stop flight?
+                    </label>
+            </div>
+            <div>
+                <input type='checkbox' name='isBasicEconomy' id='isBasicEconomy'  />
+                    <label htmlFor='isBasicEconomy'>
+                    Basic Economy?
+                    </label>
+            </div>
+            <div>
+                <input type='checkbox' name='isRefundable' id='isRefundable' />
+                    <label htmlFor='isRefundable'>
+                        Refundable?
+                    </label>
+            </div>
+            <div className="md:col-span-3">
+                <button disabled={predictData.loading} className={`${btnClassName} float-right`} type="submit">{btnLabel}</button>  
+            </div>          
         </div>
-        <div>
-            <input type='checkbox' name='isBasicEconomy' id='isBasicEconomy'  />
-                <label htmlFor='isBasicEconomy'>
-                Basic Economy?
-                </label>
-        </div>
-        <div>
-            <input type='checkbox' name='isRefundable' id='isRefundable' />
-                <label htmlFor='isRefundable'>
-                    Refundable?
-                </label>
-        </div>
-        <button disabled={predictData.loading} className={btnClassName} type="submit">{btnLabel}</button>
+
     </form>
         {(predictData && predictData.predictions && predictData.predictions.length > 0) && predictData.predictions.map((pred, idx)=>{
             return <div key={idx}>
